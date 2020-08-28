de0d98e0da4ea2d8364f739ded8a01a4c8d80eef
ioannapl <ioannaplati.9@gmail.com>
2020-08-28 17:54:00 +0300

    populate the client list with the use of react hooks and without writing classes

--- react-frontend/src/components/ClientListComponent.jsx
+++ react-frontend/src/components/ClientListComponent.jsx
@@ -1,53 +1,44 @@
-import React, {Component} from 'react'
-import ClientService from '../services/ClientService'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import ClientService from '../services/ClientService';
 
-class ClientListComponent extends Component {
-    constructor(props) {
-        super(props)
+function ClientListComponent() {
+    const[clients, setClients] = useState([]);
 
-        this.state = {
-            clients: []
-        }
-    }
-
-    componentDidMount(){
+    useEffect(() => {
         ClientService.getClients().then((res) => {
-            this.setState({clients: res.data});
+            setClients(res.data);
         });
-    }
+    });
 
-    render() {
-        return(
-            <div>
-                <h2 className='text-center'>Clients</h2>
-                <div className='row'>
-                    <table className='table table-striped table-bordered'>
-                        <thead>
-                            <tr>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Email</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.clients.map(
-                                    client => 
-                                    <tr key={client.id}>
-                                        <td>{client.firstName}</td>
-                                        <td>{client.lastName}</td>
-                                        <td>{client.email}</td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-
-                    </table>
-                </div>
+    return(
+        <div>
+            <h2 className='text-center'>Clients</h2>
+            <div className='row'>
+                <table className='table table-striped table-bordered'>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            clients.map(client => 
+                                <tr key={client.id}>
+                                    <td>{client.firstName}</td>
+                                    <td>{client.lastName}</td>
+                                    <td>{client.email}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
             </div>
-        )
-    }
+        </div>
+    );
 }
 
-export default ClientListComponent
+export default ClientListComponent;
