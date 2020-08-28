626e630c8a13a6dd48dfad10c75015b0cad8a936
ioannapl <ioannaplati.9@gmail.com>
2020-08-28 12:52:36 +0300

    basic project structure for frontend with react

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ClientListComponent from './components/ClientListComponent';
+
+function App() {
+    return (
+        <div className ='container'>
+           <ClientListComponent/> 
+        </div>
+    );
+}
+
+export default App;
---
+++ react-frontend/src/components/ClientListComponent.jsx
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+
+class ClientListComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clients: []
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h2 className='text-center'>Clients</h2>
+                <div className='row'>
+                    <table className='table table-striped table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.clients.map(
+                                    client => 
+                                    <tr key={client.id}>
+                                        <td>{client.firstName}</td>
+                                        <td>{client.lastName}</td>
+                                        <td>{client.email}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ClientListComponent
